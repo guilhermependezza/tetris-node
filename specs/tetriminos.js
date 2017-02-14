199ebb5046ff13500a8569cbcffb77a5371ff7e7
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2017-02-14 14:51:25 -0200

    Group tests

--- specs/tetriminos.js
+++ specs/tetriminos.js
@@ -9,7 +9,7 @@ describe('tetriminos', () => {
     [0, 1, 1, 0, 0],
     [0, 0, 0, 0, 0]
   ];
-  
+
   const tetriminoJ90DegClockwise = [
     [0, 0, 0, 0, 0],
     [0, 1, 0, 0, 0],
@@ -34,28 +34,30 @@ describe('tetriminos', () => {
     [0, 0, 0, 0, 0]
   ];
 
-  it('should get tetriminos', () => {
-    let tetrimino = tetriminosFactory.get('J');
-    expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ))).toBe(true);
-  });
+  describe('tetriminos rotation on creation', () => {
+    it('should get tetriminos', () => {
+      let tetrimino = tetriminosFactory.get('J');
+      expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ))).toBe(true);
+    });
 
-  it('should get a tetrimino rotated 90 degrees clockwise', () => {
-    let tetrimino = tetriminosFactory.get('J', 90);
-    expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ90DegClockwise))).toBe(true);
-  });
+    it('should get a normal tetrimino when asks for a 0 deg rotation', () => {
+      let tetrimino = tetriminosFactory.get('J', 0);
+      expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ))).toBe(true);
+    });
 
-  it('should get a tetrimino rotated 180 degrees clockwise', () => {
-    let tetrimino = tetriminosFactory.get('J', 180);
-    expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ180DegClockwise))).toBe(true);
-  });
+    it('should get a tetrimino rotated 90 degrees clockwise', () => {
+      let tetrimino = tetriminosFactory.get('J', 90);
+      expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ90DegClockwise))).toBe(true);
+    });
 
-  it('should get a normal tetrimino when asks for a 0 deg rotation', () => {
-    let tetrimino = tetriminosFactory.get('J', 0);
-    expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ))).toBe(true);
-  });
+    it('should get a tetrimino rotated 180 degrees clockwise', () => {
+      let tetrimino = tetriminosFactory.get('J', 180);
+      expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ180DegClockwise))).toBe(true);
+    });
 
-  it('should get a tetrimino rotated 270 degrees clockwise', () => {
-    let tetrimino = tetriminosFactory.get('J', 270);
-    expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ270DegClockwise), true)).toBe(true)
-  })
+    it('should get a tetrimino rotated 270 degrees clockwise', () => {
+      let tetrimino = tetriminosFactory.get('J', 270);
+      expect(tetrimino.isDataEqualTo(new Tetrimino(tetriminoJ270DegClockwise), true)).toBe(true)
+    });
+  });  
 })
