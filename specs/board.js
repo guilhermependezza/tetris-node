3f8bc4bc53ddc0ea79757791295ebd445d304ab0
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2017-02-15 11:50:10 -0200

    Implement load of initial data on board

--- specs/board.js
+++ specs/board.js
@@ -1,12 +1,33 @@
 const config = require('../src/game/config');
 const Board = require('../src/game/board');
+const { boardConfig } = config;
+let game;
+
+beforeEach(() => { game = new Board(boardConfig); })
 
 describe('board', () => {
   it('should have size defined by config file', () => {
-    const { boardConfig } = config;
-    const game = new Board(boardConfig);
     const boardSize = game.getSize();
     expect(boardSize.width).toBe(boardConfig.width);
     expect(boardSize.height).toBe(boardConfig.height);
+  });
+
+  it('should contain a 2 dimensional array to hold tetriminos data', () => {
+    const boardData = game.getData();
+    expect(boardData.constructor).toBe(Array);
+    boardData.forEach(i => { expect(i.constructor).toBe(Array) });
+  });
+
+  it('board data should have config dimensions', () => {
+    const fakeConfig = { height: 50, width: 30 };
+    const fakeBoard = new Board(fakeConfig);
+    const boardData = fakeBoard.getData();
+    expect(boardData.length).toBe(fakeConfig.height);
+    boardData.forEach(i => { expect(i.length).toBe(fakeConfig.width); });
+  });
+
+  it('initial board data should be filled with zeroes', () => {
+    const boardData = game.getData();
+    boardData.forEach(r => r.forEach(c => expect(c).toBe(0)));
   })
 })
--- src/game/board/index.js
+++ src/game/board/index.js
@@ -1,6 +1,7 @@
 function Board({ height, width }) {
   this._height = height;
   this._width = width;
+  this._data = generateInitialData(height, width);
 }
 
 Board.prototype.getSize = function() {
@@ -8,6 +9,18 @@ Board.prototype.getSize = function() {
     width: this._width,
     height: this._height
   }
+};
+
+Board.prototype.getData = function () {
+  return this._data;
+}
+
+function generateInitialData(height, width) {
+  const initialData = [];
+  const rowData = [];
+  for(let i = 0; i < width; i++) rowData.push(0);
+  for(let i = 0; i < height; i++) initialData.push([].concat(rowData));
+  return initialData;
 }
 
 module.exports = Board;
